476250f14432f65ef5228dc7b0e6a9cad3d16fd4
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-07-16 21:00:58 -0300

    configuração dos arquivos inicias

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Hero from './components/hero/Hero'
+import Jobs from './components/jobs/Jobs'
+import Nav from './components/nav/Nav'
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+    <Experience/>
+    <Jobs/>
+    <Contact/>
+    </>
+  );
+}
+
+export default App;
